572cb70766d0450d12562190a30a4225d408fd5f
Trung Anh <anhthtservice@gmail.com>
2025-05-30 09:35:41 +0700

    feat: add mainfest

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -3,9 +3,19 @@ import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getQueryClient } from "@/lib/get-query-client";
 import { getProducts } from "@/features/product/action";
 import HomePage from "@/components/local/home-screen/home-page";
+import { Locale } from "next-intl";
+import { setRequestLocale } from "next-intl/server";
 
-export default async function Home() {
+type Props = {
+  params: Promise<{ locale: Locale }>;
+};
+
+export default async function Home({ params }: Props) {
   const queryClient = getQueryClient();
+  const { locale } = await params;
+
+  // Enable static rendering
+  setRequestLocale(locale);
 
   const data = await queryClient.fetchQuery({
     queryKey: [QUERYKEYS.product],
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import { ReactNode } from "react";
-import "./globals.css";
 
 type Props = {
   children: ReactNode;
 };
 
 export default function RootLayout({ children }: Props) {
-  return (
-    <html>
-      <body>{children}</body>
-    </html>
-  );
+  return children;
 }
